a03f6a6beded462989bffad81db3560b8e46859d
waleed <waleedahmad.sidd@gmail.com>
2023-07-31 05:15:50 -0700

    31 july updated

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import "./components/css/animate.min.css";
-import "./components/css/demo.css";
-import './components/css/light-bootstrap-dashboard-react.css'
-import './components/css/light-bootstrap-dashboard-react.css.map';
-import './components/css/light-bootstrap-dashboard-react.min.css';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import "./components/css/animate.min.css";
+// import "./components/css/demo.css";
+// import './components/css/light-bootstrap-dashboard-react.css'
+// import './components/css/light-bootstrap-dashboard-react.css.map';
+// import './components/css/light-bootstrap-dashboard-react.min.css';
 
 // import './index.css';
-import App from './App';
+import App from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
